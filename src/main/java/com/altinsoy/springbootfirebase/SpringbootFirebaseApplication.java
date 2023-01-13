bc815f29f57e394956ea97d8be59c318f678bed6
Umut Cagri Altinsoy <umutcagrialtinsoy@Umuts-MacBook-Pro.local>
2023-01-13 12:10:49 +0300

    Firebase connected

---
+++ src/main/java/com/altinsoy/springbootfirebase/SpringbootFirebaseApplication.java
@@ -0,0 +1,35 @@
+package com.altinsoy.springbootfirebase;
+
+import com.google.auth.oauth2.GoogleCredentials;
+import com.google.firebase.FirebaseApp;
+import com.google.firebase.FirebaseOptions;
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+import java.io.File;
+import java.io.FileInputStream;
+import java.io.IOException;
+import java.util.Objects;
+
+@SpringBootApplication
+public class SpringbootFirebaseApplication {
+
+    public static void main(String[] args) throws IOException {
+
+        ClassLoader classLoader = SpringbootFirebaseApplication.class.getClassLoader();
+
+       // File file = new File(Objects.requireNonNull(classLoader.getResource("serviceAccountKey.json")).getFile());
+
+        FileInputStream serviceAccount =
+                new FileInputStream("./serviceAccountKey.json");
+
+
+        FirebaseOptions options = new FirebaseOptions.Builder()
+                .setCredentials(GoogleCredentials.fromStream(serviceAccount))
+                        .setDatabaseUrl("https://springboot-firebase-52114-default-rtdb.firebaseio.com")
+                                .build();
+        FirebaseApp.initializeApp(options);
+        SpringApplication.run(SpringbootFirebaseApplication.class, args);
+    }
+
+}
---
+++ src/test/java/com/altinsoy/springbootfirebase/SpringbootFirebaseApplicationTests.java
@@ -0,0 +1,13 @@
+package com.altinsoy.springbootfirebase;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SpringbootFirebaseApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
